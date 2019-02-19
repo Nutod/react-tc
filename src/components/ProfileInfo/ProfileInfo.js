e293100ec7cae4ccf5059ff6e3657604fe41a474
Nutod <alukodotun@gmail.com>
2019-02-19 13:24:41 +0100

    Completed ProfileInfo Component

--- src/components/ProfileInfo/ProfileInfo.js
+++ src/components/ProfileInfo/ProfileInfo.js
@@ -1,9 +1,43 @@
 import React from "react";
+import styled from "styled-components";
+
+const ProfileWrapper = styled.div`
+	border: 1px solid #ccc;
+	border-radius: 3px;
+	font-size: 1.3rem;
+	background: #fff;
+	margin-bottom: 0.5rem;
+
+	@media (max-width: 53em) {
+		font-size: 1.1rem;
+	}
+`;
+
+const ProfileDetails = styled.div`
+	padding: 1rem;
+	border-bottom: 1px solid #ccc;
+`;
+
+const CompleteProfileButton = styled.button`
+	background: #fff;
+	border: none;
+	color: blueviolet;
+	padding: 1rem;
+	text-transform: uppercase;
+	font-weight: 500;
+`;
 
 export default function ProfileInfo() {
 	return (
-		<div>
-			<p>Profile Info</p>
-		</div>
+		<ProfileWrapper>
+			<ProfileDetails>
+				<p>Profile Info</p>
+				<p>Range Component</p>
+				<p>Education - Where did you go to School?</p>
+				<p>Bio - Let people know more about you by adding a short bio</p>
+				<p>City - Where do you live?</p>
+			</ProfileDetails>
+			<CompleteProfileButton>Complete your Profile</CompleteProfileButton>
+		</ProfileWrapper>
 	);
 }
--- src/components/Suggestions/Suggestions.js
+++ src/components/Suggestions/Suggestions.js
@@ -1,12 +1,10 @@
 import React from "react";
+import ProfileInfo from "../ProfileInfo/ProfileInfo";
 
 export default function Suggestions() {
 	return (
 		<div>
-			Lorem ipsum dolor sit, amet consectetur adipisicing elit. Mollitia
-			accusamus rerum eum hic sequi pariatur consectetur enim perspiciatis eos
-			totam quisquam expedita soluta, ab est. Est optio nulla dolores
-			blanditiis!
+			<ProfileInfo />
 		</div>
 	);
 }
