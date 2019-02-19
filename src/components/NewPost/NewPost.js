ca865b965753f5744fecc1a18b096f386ef6e42c
Nutod <alukodotun@gmail.com>
2019-02-19 23:55:27 +0100

    Added theme file

--- src/components/NewPost/NewPost.js
+++ src/components/NewPost/NewPost.js
@@ -1,15 +1,25 @@
 import React from "react";
+import { Tabs, Tab, Box } from "grommet";
 
 export default function NewPost() {
 	return (
-		<div>
-			<p>New Post Component</p>
-			<p>
-				Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi, illum
-				modi tempore nisi laborum expedita, itaque assumenda officiis sequi sunt
-				voluptatum accusantium, rem inventore enim dolorum. Voluptatem officia
-				fugit asperiores!
-			</p>
-		</div>
+		<Tabs justify="start">
+			<Tab title="QUESTIONS" color="yellow">
+				<Box pad="medium">
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit
+					vitae, commodi reprehenderit aliquam, laboriosam nulla animi
+					doloremque, necessitatibus odio consequatur quas. Sint inventore
+					dolorem mollitia, odio recusandae quibusdam assumenda magnam.
+				</Box>
+			</Tab>
+			<Tab title="INSIGHTS">
+				<Box pad="medium">
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto,
+					incidunt! Beatae vitae suscipit est, ratione officiis tenetur culpa ad
+					magnam quis pariatur autem in maiores maxime accusamus? Cupiditate,
+					corporis soluta!
+				</Box>
+			</Tab>
+		</Tabs>
 	);
 }
