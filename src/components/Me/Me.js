e266e3b91953398137e896be235e1354e51b7f74
Nutod <alukodotun@gmail.com>
2019-02-19 11:21:28 +0100

    Added new style

--- src/components/Me/Me.js
+++ src/components/Me/Me.js
@@ -1,7 +1,12 @@
 import React from "react";
 import styled from "styled-components";
 
-const MeWrapper = styled.div``;
+const MeWrapper = styled.div`
+	display: flex;
+	flex-direction: column;
+	border: 1px solid #ccc;
+	border-radius: 3px;
+`;
 
 export default function Me() {
 	return (
