f2c83f68fea854d82ee5c78bf8451b1af0fb070a
Nutod <alukodotun@gmail.com>
2019-02-19 07:14:57 +0100

    Added New Style

--- src/container/Layout.js
+++ src/container/Layout.js
@@ -1,24 +1,26 @@
 import React, { Component } from "react";
-import { Media } from "react-breakpoints";
-import Foo from "./foo";
+// import { Media } from "react-breakpoints";
 
 export default class Layout extends Component {
 	render() {
 		return (
 			<div>
-				<Media>
-					{({ breakpoints, currentBreakpoint }) => {
-						console.log(breakpoints, currentBreakpoint);
-						if (currentBreakpoint === "mobile") {
-							return <p>Mobile</p>;
-						} else if (currentBreakpoint === "tablet") {
-							return <p>Tbalet</p>;
-						} else if (currentBreakpoint === "desktop") {
-							return <Foo />;
-						}
-					}}
-				</Media>
+				<p>HEADER</p>
+				<p>CONTENT</p>
 			</div>
 		);
 	}
 }
+
+// <Media>
+// 	{({ breakpoints, currentBreakpoint }) => {
+// 		console.log(breakpoints, currentBreakpoint);
+// 		if (currentBreakpoint === "mobile") {
+// 			return <p>Mobile</p>;
+// 		} else if (currentBreakpoint === "tablet") {
+// 			return <p>Tbalet</p>;
+// 		} else if (currentBreakpoint === "desktop") {
+// 			return <Foo />;
+// 		}
+// 	}}
+// </Media>
