57d4c82d06968ca17aab345a027ae2298b5b9290
Nutod <alukodotun@gmail.com>
2019-02-19 14:24:29 +0100

    Added FooterNav Component

--- src/components/Suggestions/Suggestions.js
+++ src/components/Suggestions/Suggestions.js
@@ -1,12 +1,14 @@
 import React from "react";
 import ProfileInfo from "../ProfileInfo/ProfileInfo";
 import Follow from "../Follow/Follow";
+import Experts from "../Experts/Experts";
 
 export default function Suggestions() {
 	return (
 		<div>
 			<ProfileInfo />
 			<Follow />
+			<Experts />
 		</div>
 	);
 }
