5758f2a73384cc6fed947d9d945b086eda067fb3
Nutod <alukodotun@gmail.com>
2019-02-20 10:07:27 +0100

    Completed Profile Dropdown

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -2,7 +2,20 @@ import React from "react";
 import styled from "styled-components";
 import { Media } from "react-breakpoints";
 import { Menu } from "grommet";
-import { MailOption, Notification, Apps } from "grommet-icons";
+import {
+	MailOption,
+	Notification,
+	Menu as MenuIcon,
+	User as UserIcon,
+	Mail as MailIcon,
+	SettingsOption,
+	StatusInfo,
+	Article,
+	Help,
+	Phone,
+	DocumentText,
+	Logout
+} from "grommet-icons";
 import Logo from "../Logo/Logo";
 import { AppContext } from "../../hoc/AppContext";
 import User from "../../assets/images/user1.jpg";
@@ -144,12 +157,152 @@ function Profile() {
 	return (
 		<Menu
 			label=""
+			dropBackground="#555"
 			items={[
-				{ label: "First Action", onClick: () => {} },
-				{ label: "Second Action", onClick: () => {} }
+				{
+					label: <ProfileUser label="Profile" />,
+					onClick: () => {}
+				},
+				{
+					label: <ProfileMessages label="Messages" />,
+					onClick: () => {}
+				},
+				{
+					label: <ProfileSettings label="Settings" />,
+					onClick: () => {}
+				},
+				{
+					label: <ProfileAbout label="About" />,
+					onClick: () => {}
+				},
+				{
+					label: <ProfileNews label="News" />,
+					onClick: () => {}
+				},
+				{
+					label: <ProfileFAQ label="FAQ" />,
+					onClick: () => {}
+				},
+				{
+					label: <ProfileContact label="Contact" />,
+					onClick: () => {}
+				},
+				{
+					label: <ProfileLegal label="Legal" />,
+					onClick: () => {}
+				},
+				{
+					label: <ProfileSignOut label="Sign Out" />,
+					onClick: () => {}
+				}
 			]}
 			icon={<Image src={User} alt="User" />}
 		/>
+	);
+}
+
+const ProfileMenuWrapper = styled.div`
+	display: flex;
+	align-items: center;
+	color: #fff;
+
+	& > * {
+		font-size: 1.3rem;
+		margin: 0 1rem;
+	}
+
+	p {
+		margin-left: 1rem;
+	}
+`;
+
+function ProfileUser({ label }) {
+	return (
+		<ProfileMenuWrapper>
+			<UserIcon size="small" color="#fff" />
+			<p>{label}</p>
+		</ProfileMenuWrapper>
+	);
+}
+
+function ProfileMessages({ label }) {
+	return (
+		<ProfileMenuWrapper>
+			<MailIcon size="small" color="#fff" />
+			<p>{label}</p>
+		</ProfileMenuWrapper>
+	);
+}
+
+function ProfileNotification({ label }) {
+	return (
+		<ProfileMenuWrapper>
+			<Notification size="small" color="#fff" />
+			<p>{label}</p>
+		</ProfileMenuWrapper>
+	);
+}
+
+function ProfileSettings({ label }) {
+	return (
+		<ProfileMenuWrapper border>
+			<SettingsOption size="small" color="#fff" />
+			<p>{label}</p>
+		</ProfileMenuWrapper>
+	);
+}
+
+function ProfileAbout({ label }) {
+	return (
+		<ProfileMenuWrapper>
+			<StatusInfo size="small" color="#fff" />
+			<p>{label}</p>
+		</ProfileMenuWrapper>
+	);
+}
+
+function ProfileNews({ label }) {
+	return (
+		<ProfileMenuWrapper>
+			<Article size="small" color="#fff" />
+			<p>{label}</p>
+		</ProfileMenuWrapper>
+	);
+}
+
+function ProfileFAQ({ label }) {
+	return (
+		<ProfileMenuWrapper>
+			<Help size="small" color="#fff" />
+			<p>{label}</p>
+		</ProfileMenuWrapper>
+	);
+}
+
+function ProfileContact({ label }) {
+	return (
+		<ProfileMenuWrapper>
+			<Phone size="small" color="#fff" />
+			<p>{label}</p>
+		</ProfileMenuWrapper>
+	);
+}
+
+function ProfileLegal({ label }) {
+	return (
+		<ProfileMenuWrapper>
+			<DocumentText size="small" color="#fff" />
+			<p>{label}</p>
+		</ProfileMenuWrapper>
+	);
+}
+
+function ProfileSignOut({ label }) {
+	return (
+		<ProfileMenuWrapper>
+			<Logout size="small" color="#fff" />
+			<p>{label}</p>
+		</ProfileMenuWrapper>
 	);
 }
 
@@ -210,9 +363,15 @@ export default function Navigation() {
 													{
 														label: <Image src={User} alt="User" />,
 														onClick: () => {}
+													},
+													{
+														label: (
+															<Button onClick={openModal}>New Post</Button>
+														),
+														onClick: () => {}
 													}
 												]}
-												icon={<Apps color="white" />}
+												icon={<MenuIcon color="white" />}
 											/>
 										</ul>
 									</MobileNavigationWrapper>
