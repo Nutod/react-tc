9e40ff3a540d58f495accb647d7ab19c2a325ed5
Nutod <alukodotun@gmail.com>
2019-02-20 02:57:15 +0100

    Added Post Logic

--- src/components/NewPost/NewPost.js
+++ src/components/NewPost/NewPost.js
@@ -1,20 +1,69 @@
 import React from "react";
 import { Tabs, Tab, Box } from "grommet";
+import styled from "styled-components";
+
+const Form = styled.form`
+	display: flex;
+	flex-direction: column;
+
+	label {
+		color: #aaa;
+		margin-top: 1.4rem;
+		font-size: 1rem;
+	}
+`;
+
+const FormField = styled.input`
+	width: 40vw;
+	border: none;
+	border-bottom: 1px solid #ccc;
+	padding: 1rem 0;
+	outline: none;
+
+	@media (max-width: 40em) {
+		width: 70vw;
+	}
+`;
+
+const Button = styled.button`
+	align-self: flex-end;
+	margin-top: 1.4rem;
+	color: #fff;
+	background: green;
+	border: none;
+	border-radius: 3px;
+	text-transform: uppercase;
+	padding: 1rem;
+	cursor: pointer;
+`;
 
 export default function NewPost() {
 	return (
 		<Tabs justify="start">
 			<Tab title="QUESTIONS" color="yellow">
 				<Box pad="medium">
-					<p>Content</p>
+					<Form>
+						<label htmlFor="topic">Topic</label>
+						<FormField name="topic" type="text" placeholder="Type Here" />
+						<label htmlFor="question">Question</label>
+						<FormField name="question" type="text" placeholder="Type Here" />
+						<label htmlFor="mention">
+							@mention someone to ask for an answer
+						</label>
+						<FormField name="mention" type="text" placeholder="Type Here" />
+						<Button type="submit">Post</Button>
+					</Form>
 				</Box>
 			</Tab>
 			<Tab title="INSIGHTS">
 				<Box pad="medium">
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto,
-					incidunt! Beatae vitae suscipit est, ratione officiis tenetur culpa ad
-					magnam quis pariatur autem in maiores maxime accusamus? Cupiditate,
-					corporis soluta!
+					<Form>
+						<label htmlFor="topic">Topic</label>
+						<FormField name="topic" type="text" placeholder="Type Here" />
+						<label htmlFor="insight">Insight</label>
+						<FormField name="insight" type="text" placeholder="Type Here" />
+						<Button type="submit">Post</Button>
+					</Form>
 				</Box>
 			</Tab>
 		</Tabs>
