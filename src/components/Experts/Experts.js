65ace3b4f95c333e9967a4d8c926a3c23716b8a2
Nutod <alukodotun@gmail.com>
2019-02-22 10:45:34 +0100

    Added ExpertsWrapper file

--- src/components/Experts/Experts.js
+++ src/components/Experts/Experts.js
@@ -1,55 +1,14 @@
 import React from "react";
 import styled from "styled-components";
 
-const ExpertsWrapper = styled.div`
-	border: 1px solid #ccc;
-	border-radius: 3px;
-	font-size: 1.3rem;
-	background: #fff;
-	margin-bottom: 1rem;
-	display: flex;
-	flex-direction: column;
-	align-items: center;
-	padding: 1rem;
-
-	@media (max-width: 53em) {
-		font-size: 1.1rem;
-	}
-
-	h3 {
-		font-weight: 200;
-		font-size: 2rem;
-
-		span {
-			color: #127bdd;
-		}
-	}
-
-	p {
-		margin-top: -1.5rem;
-		position: relative;
-
-		&::before {
-			content: "";
-			position: absolute;
-			top: 40%;
-			left: -20%;
-			background: green;
-			height: 0.6rem;
-			width: 0.6rem;
-			border-radius: 50%;
-		}
-	}
-`;
-
-export default function Experts() {
-	return (
-		<ExpertsWrapper>
-			<h3>
-				<span>1,231 </span>
-				Experts
-			</h3>
-			<p>11 new</p>
-		</ExpertsWrapper>
-	);
-}
+// export default function Experts() {
+// 	return (
+// 		<ExpertsWrapper>
+// 			<h3>
+// 				<span>1,231 </span>
+// 				Experts
+// 			</h3>
+// 			<p>11 new</p>
+// 		</ExpertsWrapper>
+// 	);
+// }
---
+++ src/components/styles/ExpertsWrapper.js
@@ -0,0 +1,40 @@
+// export const ExpertsWrapper = styled.div`
+// 	border: 1px solid #ccc;
+// 	border-radius: 3px;
+// 	font-size: 1.3rem;
+// 	background: #fff;
+// 	margin-bottom: 1rem;
+// 	display: flex;
+// 	flex-direction: column;
+// 	align-items: center;
+// 	padding: 1rem;
+
+// 	@media (max-width: 53em) {
+// 		font-size: 1.1rem;
+// 	}
+
+// 	h3 {
+// 		font-weight: 200;
+// 		font-size: 2rem;
+
+// 		span {
+// 			color: #127bdd;
+// 		}
+// 	}
+
+// 	p {
+// 		margin-top: -1.5rem;
+// 		position: relative;
+
+// 		&::before {
+// 			content: "";
+// 			position: absolute;
+// 			top: 40%;
+// 			left: -20%;
+// 			background: green;
+// 			height: 0.6rem;
+// 			width: 0.6rem;
+// 			border-radius: 50%;
+// 		}
+// 	}
+// `;
