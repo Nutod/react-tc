62666a6e10d8df139504437393252998aaba9d42
Nutod <alukodotun@gmail.com>
2019-02-22 10:46:12 +0100

    Added styles to ExpertsWrapper

--- src/components/styles/ExpertsWrapper.js
+++ src/components/styles/ExpertsWrapper.js
@@ -1,40 +1,42 @@
-// export const ExpertsWrapper = styled.div`
-// 	border: 1px solid #ccc;
-// 	border-radius: 3px;
-// 	font-size: 1.3rem;
-// 	background: #fff;
-// 	margin-bottom: 1rem;
-// 	display: flex;
-// 	flex-direction: column;
-// 	align-items: center;
-// 	padding: 1rem;
+import styled from "styled-components";
 
-// 	@media (max-width: 53em) {
-// 		font-size: 1.1rem;
-// 	}
+export const ExpertsWrapper = styled.div`
+	border: 1px solid #ccc;
+	border-radius: 3px;
+	font-size: 1.3rem;
+	background: #fff;
+	margin-bottom: 1rem;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	padding: 1rem;
 
-// 	h3 {
-// 		font-weight: 200;
-// 		font-size: 2rem;
+	@media (max-width: 53em) {
+		font-size: 1.1rem;
+	}
 
-// 		span {
-// 			color: #127bdd;
-// 		}
-// 	}
+	h3 {
+		font-weight: 200;
+		font-size: 2rem;
 
-// 	p {
-// 		margin-top: -1.5rem;
-// 		position: relative;
+		span {
+			color: #127bdd;
+		}
+	}
 
-// 		&::before {
-// 			content: "";
-// 			position: absolute;
-// 			top: 40%;
-// 			left: -20%;
-// 			background: green;
-// 			height: 0.6rem;
-// 			width: 0.6rem;
-// 			border-radius: 50%;
-// 		}
-// 	}
-// `;
+	p {
+		margin-top: -1.5rem;
+		position: relative;
+
+		&::before {
+			content: "";
+			position: absolute;
+			top: 40%;
+			left: -20%;
+			background: green;
+			height: 0.6rem;
+			width: 0.6rem;
+			border-radius: 50%;
+		}
+	}
+`;
